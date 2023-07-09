6f5663c2153a00e6bbec50979ee3e34130d3d46b
TejasBarge02 <barge237@gmail.com>
2023-07-09 17:29:34 +0530

    navbar responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
     {
       id:2,
       link: 'about'
-    },
+    }, 
     {
       id:3,
       link: 'portfolio'
@@ -42,9 +42,24 @@ const Navbar = () => {
               </li>
             ))}
         </ul>
-        <div onClick={() => setNav(!nav)} className='cursor-pointer pr-4 z-10 text-gray-500 hover:text-white flex justify-end'>
+        {nav && (
+          <ul className=" flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 ">
+            {links.map(({id, link}) => (
+                <li
+                  key={id}
+                  className='px-4 cursor-pointer capitalize py-6 text-4xl text-gray-500 hover:scale-105 hover:text-white duration-200'
+                >
+                  {link}
+                </li>
+              ))}
+          </ul>
+        )}
+
+        <div onClick={() => setNav(!nav)} className='cursor-pointer pr-4 z-10 text-gray-500 hover:text-white flex justify-end md:hidden'>
           {nav ? <FaTimes size={30}/> :<FaBars size={30}/>}
         </div>
+
+        
       
     </div>
   )
